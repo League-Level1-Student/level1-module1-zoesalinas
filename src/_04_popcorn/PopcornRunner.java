dfd09fc727e1a2af01a16ee12cadd76cde2d41d0
zoesalinas <53129571+zoesalinas@users.noreply.github.com>
2020-09-01 09:18:01 -0700

    Downloaded eclipse

--- src/_04_popcorn/PopcornRunner.java
+++ src/_04_popcorn/PopcornRunner.java
@@ -1,5 +1,10 @@
 package _04_popcorn;
-
+/* Your mission and you have to accept it:
+ * Create a PopcornMaker class (in the popcorn package) and add a main method to it. 
+ * The main method should create a bag of Popcorn and cook it in the microwave.
+ * Ask the user for the flavor of the popcorn and the number of minutes to cook it.
+ * Don't change the existing methods.
+ */
 public class PopcornRunner {
 public static void main(String[] args) {
 	
